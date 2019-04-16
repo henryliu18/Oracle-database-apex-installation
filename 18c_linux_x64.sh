4f6d3c40811ab4c0978e78d53f57e6951eba9c84
henryliu18 <45472005+henryliu18@users.noreply.github.com>
2019-04-16 15:13:14 +1000

    Update 18c_linux_x64.sh

--- 18c_linux_x64.sh
+++ 18c_linux_x64.sh
@@ -1,3 +1,22 @@
+#!/usr/bin/bash
+
+IP=1.2.3.4
+
 #/etc/hosts configuration
-echo "<IP> <HOSTNAME>" >> /etc/hosts
+echo "$IP `hostname`" >> /etc/hosts
+
+echo "fs.file-max = 6815744
+kernel.sem = 250 32000 100 128
+kernel.shmmni = 4096
+kernel.shmall = 1073741824
+kernel.shmmax = 4398046511104
+kernel.panic_on_oops = 1
+net.core.rmem_default = 262144
+net.core.rmem_max = 4194304
+net.core.wmem_default = 262144
+net.core.wmem_max = 1048576
+net.ipv4.conf.all.rp_filter = 2
+net.ipv4.conf.default.rp_filter = 2
+fs.aio-max-nr = 1048576
+net.ipv4.ip_local_port_range = 9000 65500" >> /etc/sysctl.conf
 
