33fcd81351e00b82ae178cbae8b1a103ee54801a
henryliu18 <45472005+henryliu18@users.noreply.github.com>
2019-04-16 15:23:01 +1000

    Update 18c_linux_x64.sh

--- 18c_linux_x64.sh
+++ 18c_linux_x64.sh
@@ -5,6 +5,7 @@ IP=1.2.3.4
 #/etc/hosts configuration
 echo "$IP `hostname`" >> /etc/hosts
 
+#Configuring the Kernel Parameter Settings
 echo "fs.file-max = 6815744
 kernel.sem = 250 32000 100 128
 kernel.shmmni = 4096
@@ -19,4 +20,66 @@ net.ipv4.conf.all.rp_filter = 2
 net.ipv4.conf.default.rp_filter = 2
 fs.aio-max-nr = 1048576
 net.ipv4.ip_local_port_range = 9000 65500" >> /etc/sysctl.conf
+
+#Run the following command to change the current kernel parameters.
+/sbin/sysctl -p
+
+#Setting Shell Limits for the Oracle User
+echo "oracle   soft   nofile    1024
+oracle   hard   nofile    65536
+oracle   soft   nproc    16384
+oracle   hard   nproc    16384
+oracle   soft   stack    10240
+oracle   hard   stack    32768
+oracle   hard   memlock    134217728
+oracle   soft   memlock    134217728" > /etc/security/limits.conf
+
+#The following packages are listed as required, including the 32-bit version of some of the packages. Many of the packages should be installed already.
+yum install -y bc    
+yum install -y binutils
+yum install -y compat-libcap1
+yum install -y compat-libstdc++-33
+yum install -y compat-libstdc++-33.i686
+yum install -y elfutils-libelf.i686
+yum install -y elfutils-libelf
+yum install -y elfutils-libelf-devel.i686
+yum install -y elfutils-libelf-devel
+yum install -y fontconfig-devel
+yum install -y glibc.i686
+yum install -y glibc
+yum install -y glibc-devel.i686
+yum install -y glibc-devel
+yum install -y ksh
+yum install -y libaio.i686
+yum install -y libaio
+yum install -y libaio-devel.i686
+yum install -y libaio-devel
+yum install -y libX11.i686
+yum install -y libX11
+yum install -y libXau.i686
+yum install -y libXau
+yum install -y libXi.i686
+yum install -y libXi
+yum install -y libXtst.i686
+yum install -y libXtst
+yum install -y libgcc.i686
+yum install -y libgcc
+yum install -y librdmacm-devel
+yum install -y libstdc++.i686
+yum install -y libstdc++
+yum install -y libstdc++-devel.i686
+yum install -y libstdc++-devel
+yum install -y libxcb.i686
+yum install -y libxcb
+yum install -y make
+yum install -y nfs-utils
+yum install -y net-tools
+yum install -y python
+yum install -y python-configshell
+yum install -y python-rtslib
+yum install -y python-six
+yum install -y smartmontools
+yum install -y sysstat
+yum install -y targetcli
+yum install -y unixODBC
 
