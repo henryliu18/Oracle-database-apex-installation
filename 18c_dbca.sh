dd1684e095c9fcbac4a8cfe1d97d11ddb0d7dd26
henryliu18 <45472005+henryliu18@users.noreply.github.com>
2019-04-16 22:14:51 +1000

    Create 18c_dbca.sh

---
+++ 18c_dbca.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/bash
+
+#
+# Tested CentOS 7
+# Database creation, run as oracle user
+#
+
+
+dbca -silent -createDatabase                                                   \
+     -templateName General_Purpose.dbc                                         \
+     -gdbname ${ORACLE_SID} -sid  ${ORACLE_SID} -responseFile NO_VALUE         \
+     -characterSet AL32UTF8                                                    \
+     -sysPassword SysPassword1                                                 \
+     -systemPassword SysPassword1                                              \
+     -createAsContainerDatabase true                                           \
+     -numberOfPDBs 1                                                           \
+     -pdbName ${PDB_NAME}                                                      \
+     -pdbAdminPassword PdbPassword1                                            \
+     -databaseType MULTIPURPOSE                                                \
+     -automaticMemoryManagement false                                          \
+     -totalMemory 2000                                                         \
+     -storageType FS                                                           \
+     -datafileDestination "${DATA_DIR}"                                        \
+     -redoLogFileSize 50                                                       \
+     -emConfiguration NONE                                                     \
+     -ignorePreReqs
