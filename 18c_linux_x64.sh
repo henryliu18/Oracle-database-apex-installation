b4ea212cf4079f6bd5448911046feebee528bb38
henryliu18 <45472005+henryliu18@users.noreply.github.com>
2019-04-16 15:59:08 +1000

    Update 18c_linux_x64.sh

--- 18c_linux_x64.sh
+++ 18c_linux_x64.sh
@@ -147,8 +147,8 @@ export ORACLE_SID=cdb1
 export PDB_NAME=pdb1
 export DATA_DIR=$ORACLE_DB
 
-export PATH=/usr/sbin:/usr/local/bin:$PATH
-export PATH=$ORACLE_HOME/bin:$PATH
+export PATH=/usr/sbin:/usr/local/bin:\$PATH
+export PATH=$ORACLE_HOME/bin:\$PATH
 
 export LD_LIBRARY_PATH=$ORACLE_HOME/lib:/lib:/usr/lib
 export CLASSPATH=$ORACLE_HOME/jlib:$ORACLE_HOME/rdbms/jlib" >> /home/$O_USER/.bash_profile
