0738c18cf14dd8f201ee4fd12f3ffc22136b9832
henryliu18 <45472005+henryliu18@users.noreply.github.com>
2019-04-19 00:34:13 +1000

    Delete 11gR2_netca.sh

--- 11.2/11gR2_netca.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-
-#
-# Tested CentOS Linux release 7.6.1810 (Core)
-# Listener configuration, run as oracle user
-#
-
-netca /silent /responsefile $ORACLE_HOME/assistants/netca/netca.rsp
