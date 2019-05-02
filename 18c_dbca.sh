b436bd1e2fd433226b1157cf3716b80e2473a9c3
henryliu18 <45472005+henryliu18@users.noreply.github.com>
2019-05-02 21:02:48 +1000

    Database creation can be run as root now

--- 18c_dbca.sh
+++ 18c_dbca.sh
@@ -2,29 +2,37 @@
 
 #
 # Tested CentOS 7
-# Database creation, run as oracle user
+# Database creation, run as root user
 #
+
 ORATAB=/etc/oratab
 TMPORATAB=/tmp/oratab
+O_USER=oracle
+RUN_SCRIPT=/tmp/run_dbca
 
-dbca -silent -createDatabase                                                   \
-     -templateName General_Purpose.dbc                                         \
-     -gdbname ${ORACLE_SID} -sid  ${ORACLE_SID} -responseFile NO_VALUE         \
-     -characterSet AL32UTF8                                                    \
-     -sysPassword SysPassword1                                                 \
-     -systemPassword SysPassword1                                              \
-     -createAsContainerDatabase true                                           \
-     -numberOfPDBs 1                                                           \
-     -pdbName ${PDB_NAME}                                                      \
-     -pdbAdminPassword PdbPassword1                                            \
-     -databaseType MULTIPURPOSE                                                \
-     -automaticMemoryManagement false                                          \
-     -totalMemory 2000                                                         \
-     -storageType FS                                                           \
-     -datafileDestination "${DATA_DIR}"                                        \
-     -redoLogFileSize 50                                                       \
-     -emConfiguration NONE                                                     \
-     -ignorePreReqs
+echo "dbca -silent -createDatabase \
+     -templateName General_Purpose.dbc \
+     -gdbname \$ORACLE_SID \
+     -sid \$ORACLE_SID \
+	-responseFile NO_VALUE \
+     -characterSet AL32UTF8 \
+     -sysPassword SysPassword1 \
+     -systemPassword SysPassword1 \
+     -createAsContainerDatabase true \
+     -numberOfPDBs 1 \
+     -pdbName \$PDB_NAME \
+     -pdbAdminPassword PdbPassword1 \
+     -databaseType MULTIPURPOSE \
+     -automaticMemoryManagement false \
+     -totalMemory 1500 \
+     -storageType FS \
+     -datafileDestination "\$DATA_DIR" \
+     -redoLogFileSize 50 \
+     -emConfiguration NONE \
+     -ignorePreReqs" > $RUN_SCRIPT
+chmod a+x $RUN_SCRIPT
+su - $O_USER -c $RUN_SCRIPT
+rm -f $RUN_SCRIPT
 
 # Change auto start flag from N to Y
 sed -e "/$ORACLE_SID/s/^/#/g" $ORATAB > $TMPORATAB
