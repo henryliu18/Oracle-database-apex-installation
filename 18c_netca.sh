dad358422d558b3905ad8050df5e82a26c49b122
henryliu18 <45472005+henryliu18@users.noreply.github.com>
2019-05-02 20:02:39 +1000

    Now this script can be run as root

--- 18c_netca.sh
+++ 18c_netca.sh
@@ -2,8 +2,13 @@
 
 #
 # Tested CentOS 7
-# Listener configuration, run as oracle user
+# Listener configuration, run as root user
 #
 
-netca /silent /responsefile $ORACLE_HOME/assistants/netca/netca.rsp
+O_USER=oracle
+RUN_SCRIPT=/tmp/run_netca
 
+echo "netca /silent /responsefile \$ORACLE_HOME/assistants/netca/netca.rsp" > $RUN_SCRIPT
+chmod a+x $RUN_SCRIPT
+su - $O_USER -c $RUN_SCRIPT
+rm -f $RUN_SCRIPT
