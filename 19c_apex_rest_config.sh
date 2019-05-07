33b3f1a4b6fe7680d99e0d93928e09d9b962d1f7
henryliu18 <45472005+henryliu18@users.noreply.github.com>
2019-05-08 02:19:03 +1000

    Update 19c_apex_rest_config.sh

--- 19c_apex_rest_config.sh
+++ 19c_apex_rest_config.sh
@@ -13,7 +13,8 @@ echo "ORAENV_ASK=NO
 . oraenv
 
 cd $APEX_HOME/apex/
-$ORACLE_HOME/bin/sqlplus sys/SysPassword1@$PDB as sysdba @apex_rest_config.sql" > /tmp/apex_rest_config.sh
+$ORACLE_HOME/bin/sqlplus sys/SysPassword1@$PDB as sysdba @apex_rest_config.sql
+exit;" > /tmp/apex_rest_config.sh
 chmod a+x /tmp/apex_rest_config.sh
 
 su - $O_USER -c /tmp/apex_rest_config.sh
