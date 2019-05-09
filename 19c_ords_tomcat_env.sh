24424366dd6efa31ad4d472a88b9caa00b35a065
henryliu18 <45472005+henryliu18@users.noreply.github.com>
2019-05-09 19:02:28 +1000

    Update 19c_ords_tomcat_env.sh

--- 19c_ords_tomcat_env.sh
+++ 19c_ords_tomcat_env.sh
@@ -32,3 +32,19 @@ APEX_PASS=${APEX_PASS:-ApexIntPassword1}
 
 read -p "Database container name for apex [pdb1]: " PDB
 PDB=${PDB:-pdb1}
+
+echo "TOMCAT_ZIP=${TOMCAT_ZIP}
+ORDS_ZIP=${ORDS_ZIP}
+TOMCAT_HOME=${TOMCAT_HOME}
+ORDS_HOME=${ORDS_HOME}
+SYS_PASS=${SYS_PASS}
+APEX_PASS=${APEX_PASS}
+PDB=${PDB}" > `dirname $0`/ordsenv
+
+if [ $? -eq 0 ]; then
+ echo "**************************************************************"
+ echo "*** ordsenv saved in `dirname $0`/ordsenv *"
+ echo "**************************************************************"
+else
+ echo "`dirname $0`/ordsenv saved failed";
+fi
