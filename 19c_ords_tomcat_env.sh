2dac2717fb9162d7978d7ba885f1bfa6e36f0d5d
henryliu18 <45472005+henryliu18@users.noreply.github.com>
2019-05-09 19:10:52 +1000

    Update 19c_ords_tomcat_env.sh

--- 19c_ords_tomcat_env.sh
+++ 19c_ords_tomcat_env.sh
@@ -30,7 +30,13 @@ SYS_PASS=${SYS_PASS:-SysPassword1}
 read -p "APEX and ORDS internal accounts password [ApexIntPassword1]: " APEX_PASS
 APEX_PASS=${APEX_PASS:-ApexIntPassword1}
 
-read -p "Database container name for apex [pdb1]: " PDB
+read -p "Database hostname [192.168.56.102]: " ORACLE_HOSTNAME
+ORACLE_HOSTNAME=${ORACLE_HOSTNAME:-192.168.56.102}
+
+read -p "Database port [1521]: " ORACLE_PORT
+ORACLE_PORT=${ORACLE_PORT:-1521}
+
+read -p "Database name [pdb1]: " PDB
 PDB=${PDB:-pdb1}
 
 echo "TOMCAT_ZIP=${TOMCAT_ZIP}
@@ -39,6 +45,8 @@ TOMCAT_HOME=${TOMCAT_HOME}
 ORDS_HOME=${ORDS_HOME}
 SYS_PASS=${SYS_PASS}
 APEX_PASS=${APEX_PASS}
+ORACLE_HOSTNAME=${ORACLE_HOSTNAME}
+ORACLE_PORT=${ORACLE_PORT}
 PDB=${PDB}" > `dirname $0`/ordsenv
 
 if [ $? -eq 0 ]; then
