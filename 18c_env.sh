1a37ca22337bfd6f81daae23ef3955b55ca9315e
henryliu18 <45472005+henryliu18@users.noreply.github.com>
2019-05-10 11:38:00 +1000

    Update 18c_env.sh

--- 18c_env.sh
+++ 18c_env.sh
@@ -5,6 +5,7 @@ GREEN='\033[0;32m'
 OFF='\033[0m'
 
 clear
+echo "Networking"
 ifconfig -a|grep 'inet\|flags'
 read -p "Servicable network interface [eth0]: " NIC
 NIC=${NIC:-eth0}
@@ -15,6 +16,7 @@ if [ $? -ne 0 ]; then
 fi
 
 clear
+echo "Software (zip file) location"
 read -p "Oracle software zip file [/tmp/LINUX.X64_180000_db_home.zip]: " ORACLE_SW
 ORACLE_SW=${ORACLE_SW:-/tmp/LINUX.X64_180000_db_home.zip}
 if [ ! -f $ORACLE_SW ]; then
@@ -36,6 +38,8 @@ if [ ! -f $ORDS_SW ]; then
  exit 1
 fi
 
+clear
+echo "Security"
 read -p "Oracle account [oracle]: " O_USER
 O_USER=${O_USER:-oracle}
 
@@ -54,6 +58,8 @@ PDBADMIN_PASS=${PDBADMIN_PASS:-PdbPassword1}
 read -p "APEX and ORDS internal accounts password [ApexIntPassword1]: " APEX_PASS
 APEX_PASS=${APEX_PASS:-ApexIntPassword1}
 
+clear
+echo "Installation location"
 read -p "\$ORACLE_APP_ROOT (The root directory that will hold oracle database and oraInventory binaries) [/opt/app]: " ORACLE_APP_ROOT
 ORACLE_APP_ROOT=${ORACLE_APP_ROOT:-/opt/app}
 
@@ -66,33 +72,20 @@ ORACLE_HOME=${ORACLE_HOME:-\$ORACLE_BASE/product/18.0.0/dbhome_1}
 read -p "Oracle database files directory [/ora/db001]: " ORACLE_DB
 ORACLE_DB=${ORACLE_DB:-/ora/db001}
 
+read -p "Apex software home [\$ORACLE_BASE/apex19]: " APEX_HOME
+APEX_HOME=${APEX_HOME:-\$ORACLE_BASE/apex19}
+
+read -p "ORDS software home [/home/oracle/ords]: " ORDS_HOME
+ORDS_HOME=${ORDS_HOME:-/home/oracle/ords}
+
 read -p "Location of auto generated scripts during installation [/tmp]: " SCRIPT_DIR
 SCRIPT_DIR=${SCRIPT_DIR:-/tmp}
-
-#read -p "Location of auto generated script for oracle db softwarae installation [/tmp/inst_ora_sw.sh]: " INST_ORACLE_SW_SHELL
-#INST_ORACLE_SW_SHELL=${INST_ORACLE_SW_SHELL:-/tmp/inst_ora_sw.sh}
-
-#read -p "Location of auto generated script for netca [/tmp/run_netca]: " RUN_NETCA
-#RUN_NETCA=${RUN_NETCA:-/tmp/run_netca}
-
-#read -p "Location of auto generated script for dbca [/tmp/run_dbca]: " RUN_DBCA
-#RUN_DBCA=${RUN_DBCA:-/tmp/run_dbca}
 
 read -p "Location of oratab [/etc/oratab]: " ORATAB
 ORATAB=${ORATAB:-/etc/oratab}
 
-#read -p "Location of temporary oratab [/tmp/oratab]: " TMPORATAB
-#TMPORATAB=${TMPORATAB:-/tmp/oratab}
-
-#read -p "Location of Apex sql file for installation [/tmp/inst_apex.sql]: " APEX_SQL
-#APEX_SQL=${APEX_SQL:-/tmp/inst_apex.sql}
-
-#read -p "Location of auto generated script for apex installation [/tmp/run_apex]: " RUN_APEX
-#RUN_APEX=${RUN_APEX:-/tmp/run_apex}
-
-#read -p "Location of auto generated script for ORDS webapp build [/tmp/run_ords]: " RUN_ORDS
-#RUN_ORDS=${RUN_ORDS:-/tmp/run_ords}
-
+clear
+echo "Oracle Database"
 read -p "\$ORACLE_SID - Database database name or container instance name for apex [cdb1]: " CDB
 CDB=${CDB:-cdb1}
 
@@ -102,11 +95,6 @@ PDB=${PDB:-pdb1}
 read -p "Tablespace name for apex [APEX]: " APEX_TABLESPACE
 APEX_TABLESPACE=${APEX_TABLESPACE:-APEX}
 
-read -p "Apex software home [\$ORACLE_BASE/apex19]: " APEX_HOME
-APEX_HOME=${APEX_HOME:-\$ORACLE_BASE/apex19}
-
-read -p "ORDS software home [/home/oracle/ords]: " ORDS_HOME
-ORDS_HOME=${ORDS_HOME:-/home/oracle/ords}
 
 echo "NIC=${NIC}
 O_USER=${O_USER}
@@ -122,15 +110,8 @@ ORACLE_DB=${ORACLE_DB}
 ORACLE_SW=${ORACLE_SW}
 ORDS_SW=${ORDS_SW}
 SCRIPT_DIR=${SCRIPT_DIR}
-#INST_ORACLE_SW_SHELL=${INST_ORACLE_SW_SHELL}
-#RUN_NETCA=${RUN_NETCA}
-#RUN_DBCA=${RUN_DBCA}
 ORATAB=${ORATAB}
-#TMPORATAB=${TMPORATAB}
 APEX_SW=${APEX_SW}
-#APEX_SQL=${APEX_SQL}
-#RUN_APEX=${RUN_APEX}
-#RUN_ORDS=${RUN_ORDS}
 PDB=${PDB}
 CDB=${CDB}
 APEX_TABLESPACE=${APEX_TABLESPACE}
