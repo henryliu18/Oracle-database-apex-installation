11ee29be763d31bbcf32a453cbb7b97e7bbd03b0
root <root@localhost.localdomain>
2019-06-01 01:02:12 +1000

    modified:   18c_env.sh

--- 18c_env.sh
+++ 18c_env.sh
@@ -119,9 +119,87 @@ APEX_HOME=${APEX_HOME}
 ORDS_HOME=${ORDS_HOME}" > `dirname $0`/env
 
 if [ $? -eq 0 ]; then
- echo "**************************************************************"
- echo "*** env saved in `dirname $0`/env *"
- echo "**************************************************************"
+this_id=`id -u`
+OHROOT=`dirname ${ORACLE_APP_ROOT}`
+diskfree=`df $OHROOT | awk '/[0-9]%/{print $(NF-2)}'`
+if [ -f /etc/os-release ]; then
+    # freedesktop.org and systemd
+    . /etc/os-release
+    OS=$NAME
+    VER=$VERSION_ID
+elif type lsb_release >/dev/null 2>&1; then
+    # linuxbase.org
+    OS=$(lsb_release -si)
+    VER=$(lsb_release -sr)
+elif [ -f /etc/lsb-release ]; then
+    # For some versions of Debian/Ubuntu without lsb_release command
+    . /etc/lsb-release
+    OS=$DISTRIB_ID
+    VER=$DISTRIB_RELEASE
+elif [ -f /etc/debian_version ]; then
+    # Older Debian/Ubuntu/etc.
+    OS=Debian
+    VER=$(cat /etc/debian_version)
+elif [ -f /etc/SuSe-release ]; then
+    # Older SuSE/etc.
+    ...
+elif [ -f /etc/redhat-release ]; then
+    # Older Red Hat, CentOS, etc.
+    ...
+else
+    # Fall back to uname, e.g. "Linux <version>", also works for BSD, etc.
+    OS=$(uname -s)
+    VER=$(uname -r)
+fi
+
+if [ $this_id -ne 0 ]; then
+  echo -e "${RED}you are not root${OFF}"
+  exit 1
+elif [ ! "command -v unzip" ]; then
+  echo -e "${RED}unzip not found${OFF}"
+  exit 1
+elif [ ! "command -v tar" ]; then
+  echo -e "${RED}tar not found${OFF}"
+  exit 1
+elif [ ! "command -v yum" ]; then
+  echo -e "${RED}yum not found${OFF}"
+  exit 1
+elif [ "$OS" = "CentOS Linux" ] && [ ! $VER -eq 8 ] && [ ! $VER -eq 7 ] && [ ! $VER -eq 6 ]; then
+  echo -e "${RED}$OS $VER incompatible${OFF}"
+  exit 1
+elif [ ! -f ${ORACLE_SW} ]; then
+  echo -e "${RED}Oracle DB software not found${OFF}"
+  exit 1
+elif [ ! -f ${APEX_SW} ]; then
+  echo -e "${RED}APEX software not found${OFF}"
+  exit 1
+elif [ ! -f ${ORDS_SW} ]; then
+  echo -e "${RED}ORDS software not found${OFF}"
+  exit 1
+elif ! ping -q -c 1 -W 1 www.google.com >/dev/null; then
+  echo -e "${RED}IPv4 is down${OFF}"
+  exit 1
+elif [ $diskfree -lt 20000000 ]; then
+  echo -e "${RED}Storage too small${OFF}"
+  exit 1
+else
+  echo Readiness Check
+  echo "OS check..........[$OS-$VER]"
+  echo "uid check..........[$this_id]"
+  echo "unzip check..........[`command -v unzip>/dev/null;echo $?`]"
+  echo "tar check..........[`command -v tar>/dev/null;echo $?`]"
+  echo "yum check..........[`command -v yum>/dev/null;echo $?`]"
+  echo "Oracle DB software check..........[`ls ${ORACLE_SW}>/dev/null;echo $?`]"
+  echo "APEX software check..........[`ls ${APEX_SW}>/dev/null;echo $?`]"
+  echo "ORDS software check..........[`ls ${ORDS_SW}>/dev/null;echo $?`]"
+  echo "Internet check..........[`ping -q -c 1 -W 1 www.google.com>/dev/null;echo $?`]"
+  echo "Disk check..........["$OHROOT-$diskfree KB free"]"
+  echo "**************************************************************"
+  echo "*** env saved in `dirname $0`/env *"
+  echo "**************************************************************"
+  exit 0
+fi
+
 else
  echo "`dirname $0`/env saved failed";
 fi
